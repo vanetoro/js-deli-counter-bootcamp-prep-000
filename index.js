add8ce05dbd667424ca2b5efb4b1aa03d3de658b
Vanessa Toro <vanetoro84@gmail.com>
2018-01-30 01:26:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@
  
  
  function nowServing(){
-   katzDeli.shift();
+   
    if(katzDeli.length === 0){
      return 'There is nobody waiting to be served!';
    } else{
    var serving = katzDeli[0];
+   katzDeli.shift();
    return `Currently serving ${serving}`;
    }
    
